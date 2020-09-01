d8269cd1147fa8a4d0d09bd11cdd3a380546a4c1
okandavut <davut.okan@gmail.com>
2020-09-01 17:26:10 +0300

    material ui implemented

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
+import LeftContainer from "../containers/LeftContainer/leftcontainer";
+import RightContainer from "../containers/RightContainer/rightcontainer";
+import MainContainer from "../containers/MainContainer/maincontainer";
+import TweetContainer from "../containers/TweetContainer/tweetcontainer";
+import SearchBox from "../components/SearchBox/searchbox";
 
 function Home() {
   return (
-    <div className="App">
-      Home page
-    </div>
+    <Container fixed>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          Logo comes here
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TweetContainer />
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <SearchBox />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <LeftContainer />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <MainContainer />
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <RightContainer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
