cc6bb684d99f8235f8b46d78c9df44f7ccb8e4dc
okandavut <davut.okan@gmail.com>
2020-09-01 17:27:09 +0300

    Searchbox component added

---
+++ src/components/SearchBox/searchbox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBox() {
+  return (
+    <div className="App">
+        <input type="text" placeholder="Search"></input>
+    </div>
+  );
+}
+
+export default SearchBox;
